143344ad07611251e98ddb1518f1fa253f09bf63
MaksymManyk <manik.maxim@gmail.com>
2024-01-08 22:14:30 +0200

    Home Work 2.1

---
+++ HWLesson 2.1/HWLesson 2.1/Classes/Actions .cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+
+namespace HWLesson21.Classes
+{
+    public class Actions
+    {
+        public Result Start()
+        {
+            Result result = new Result();
+            result.status = true;
+            result.message = "Start method";
+            return result;
+        }
+
+        public Result Skip()
+        {
+            Result result = new Result();
+            result.status = true;
+            result.message = "Skipped logic in method";
+            return result;
+        }
+
+        public Result Broke()
+        {
+            Result result = new Result();
+            result.status = false;
+            result.message = "I broke a logic";
+            return result;
+        }
+    }
+}
---
+++ HWLesson 2.1/HWLesson 2.1/Classes/Starter.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace HWLesson21.Classes
+{
+    public class Starter
+    {
+        public void Run()
+        {
+            Logger log = Logger.Instance;
+            int random;
+            for (int i = 0; i < 100; i++)
+            {
+                random = new Random().Next(1, 4);
+                this.GenerateMessage(random);
+            }
+
+            log.PrintLog();
+            Console.WriteLine($"{Environment.NewLine}Do you want save log?: Y/N");
+            string? isSaveLog = Console.ReadLine();
+
+            if (isSaveLog.ToUpper() == "Y")
+             {
+                log.SaveLog();
+             }
+        }
+
+        public void GenerateMessage(int randomNum)
+        {
+            Logger log = Logger.Instance;
+            Actions actions = new Actions();
+            Result result = new Result();
+            switch (randomNum)
+            {
+                case 1:
+                    {
+                        result = actions.Start();
+                        log.AddLog("Info", result);
+                        break;
+                    }
+
+                case 2:
+                    {
+                        result = actions.Skip();
+                        log.AddLog("Warning", result);
+                        break;
+                    }
+
+                case 3:
+                    {
+                        result = actions.Broke();
+                        result.message = result.status == false ? "Action failed by a reason: " + result.message : result.message;
+                        log.AddLog("Error", result);
+                        break;
+                    }
+
+                default:
+                    {
+                        break;
+                    }
+            }
+        }
+    }
+}
