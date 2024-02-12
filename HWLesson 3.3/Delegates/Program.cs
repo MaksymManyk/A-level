95c0a079fc4f4ece0f60c77d6cae3aa94dddb6a1
MaksymManyk <manik.maxim@gmail.com>
2024-02-12 15:19:15 +0200

    Home Work Lesson 3.3 Delegates

---
+++ HWLesson 3.3/Delegates/Program.cs
@@ -0,0 +1,53 @@
+﻿
+namespace Delegates
+{
+    public class Mult
+    {
+        public delegate void ShowDelegate(bool is_True);    
+        public ShowDelegate ShowsDelegate { get; set; }
+        public int Multiply (int a, int b) { return a * b; }
+        
+    }
+
+    public class Calculate
+    {
+        public delegate int CalcDelegate(int a, int b);
+        public CalcDelegate CalcsDelegate { get; set; }
+
+        public delegate bool ReturnDelegate(int a);
+        public ReturnDelegate  ReturnsDelegate { get; set; }
+
+        public int Multiply;
+        public ReturnDelegate Calc(int a, int b, CalcDelegate s)
+        {
+            this.Multiply = s.Invoke(a, b);
+            ReturnDelegate result = Result;
+            return result;
+        }
+        public bool Result(int a)
+        {
+            bool result = Multiply % a == 0;
+            return result;
+        }
+
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Mult m = new Mult();
+            Calculate c  = new Calculate();
+       
+            c.CalcsDelegate = m.Multiply;
+            c.ReturnsDelegate = c.Calc(3, 10, c.CalcsDelegate);
+            m.ShowsDelegate = Show;
+
+            m.ShowsDelegate.Invoke(c.ReturnsDelegate.Invoke(5));
+        }
+        public static void Show(bool is_True)
+        {
+            Console.WriteLine(is_True);
+        }
+
+    }
+}
