9b16f895ea1e4f7b8da25c48ff1575846121bb8a
MaksymManyk <manik.maxim@gmail.com>
2024-05-18 13:46:54 +0300

    Home Work Lesson 6.2

---
+++ HWLesson 6.2/Catalog/Catalog.Host/Repositories/Interfaces/ICatalogTypeRepository.cs
@@ -0,0 +1,13 @@
+﻿using Catalog.Host.Data.Entities;
+
+namespace Catalog.Host.Repositories.Interfaces
+{
+    public interface ICatalogTypeRepository
+    {
+        Task<IEnumerable<CatalogType>> GetTypesAsync();
+
+        Task<int?> Add(string type);
+        Task Delete(int id);
+        Task Update(int id, string typeName);
+    }
+}
